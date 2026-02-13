ed59bb7e43f7ca8a7858cdebd5d3709329bf2dd0
Avinash Kakade <kakadeavinash9@gmail.com>
2026-02-13 12:23:01 +0530

    Production ready

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +9,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
